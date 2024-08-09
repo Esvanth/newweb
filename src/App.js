575c0c41915badcf9f37f8d8fd6228b56897f0c8
Esvanth M <104833732+Esvanth@users.noreply.github.com>
2024-08-09 06:37:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import data from "./TemplateData.json";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="templateContainer">
+        <div className="searchInput_Container">
+          <input
+            id="searchInput"
+            type="text"
+            placeholder="Search here..."
+            onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}
+          />
+        </div>
+        <div className="template_Container">
+          {
+            data.filter((val) => {
+              if (searchTerm === "") {
+                return true; // Return true to include this item in the filtered results
+              } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return true; // Return true to include this item if it matches the search term
+              }
+              return false; // Return false to exclude this item
+            }).map((val) => {
+              return (
+                <div className="template" key={val.id}>
+                  <img src={val.image} alt="" />
+                  <h3>{val.title}</h3>
+                  <p className="price">${val.price}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+  
